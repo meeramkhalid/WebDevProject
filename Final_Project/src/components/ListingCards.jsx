67a90c768056318d5840410ec5dd32fbd1b69213
meeramkhalid <meeramkhalid2@gmail.com>
2024-11-16 19:32:35 +0500

    Details page and booking form navigation

--- Final_Project/src/components/ListingCards.jsx
+++ Final_Project/src/components/ListingCards.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';  // Import Link from react-router-dom
 import '../components_styles/ListingCards.css'; 
 
-const ListingCard = ({ image, title, propertyType, guests, bedrooms, bathrooms, price, rating, location, amenities }) => {
+const ListingCard = ({ id, image, title, propertyType, guests, bedrooms, bathrooms, price, rating, location, amenities }) => {
   return (
     <div className="listing-card">
-      <img src={image} alt={title} className="listing-image" />
+      {/* Make the image clickable by wrapping it in a Link */}
+      <Link to={`/listing/${id}`}>
+        <img src={image} alt={title} className="listing-image" />
+      </Link>
+      
       <div className="listing-details">
         <h3 className="listing-title">{title}</h3>
         <p className="listing-type">{propertyType}</p>
